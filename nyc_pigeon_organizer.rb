efc3d0bea68561db6290c5266dd884fb4539d16c
Jodi Croft <jodipruett@gmail.com>
2020-05-22 16:23:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,17 @@
 require "pry"
 
 def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do |(color_gender_lives, inner_keys), final_array|
+  data.each_with_object({}) do |(color_gender_lives, inner_keys), final_hash|
     inner_keys.each do |inner_key, names|
-      binding.pry
       names.each do |name|
-        if !final_array[name]
-          final_array[name] = {}
+        binding.pry
+        if !final_hash[name]
+          final_hash[name] = {}
         end
-      if !final_array[name][color_gender_lives]
-        final_array[name][color_gender_lives] = []
+      if !final_hash[name][color_gender_lives]
+        final_hash[name][color_gender_lives] = []
       end
-      final_array[name][color_gender_lives].push(inner_key.to_s)
+      final_hash[name][color_gender_lives].push(inner_key.to_s)
     end
   end
 end
