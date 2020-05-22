3a5ed4b768bf62a6ebbb7c3ec107079333c41035
Jodi Croft <jodipruett@gmail.com>
2020-05-22 16:12:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,7 @@ require "pry"
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(color_gender_lives, inner_keys), final_array|
     inner_keys.each do |inner_key, names|
+      binding.pry
       names.each do |name|
         if !final_array[name]
           final_array[name] = {}
@@ -16,4 +17,3 @@ def nyc_pigeon_organizer(data)
 end
 end
 
-binding.pry
