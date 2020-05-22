ee8651be896bdc9e46cc7cfbe4241c554c489409
Jodi Croft <jodipruett@gmail.com>
2020-05-22 16:27:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,10 +6,10 @@ def nyc_pigeon_organizer(data)
       names.each do |name|
         if !final_hash[name]
           final_hash[name] = {}
-          binding.pry
         end
       if !final_hash[name][color_gender_lives]
         final_hash[name][color_gender_lives] = []
+        binding.pry
       end
       final_hash[name][color_gender_lives].push(inner_key.to_s)
     end
